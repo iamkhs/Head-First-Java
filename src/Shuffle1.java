97267273cb23891611acdef8aa4a433099ab5869
Kibriar Hossain <shantohossain.p@gmail.com>
2022-11-23 21:05:33 +0600

    Head First Java Exercise Code Magnet Solution

---
+++ src/Shuffle1.java
@@ -0,0 +1,20 @@
+public class Shuffle1 {
+    // Head First Java Code Magnet Exercise Solution
+    public static void main(String[] args){
+        int x = 3;
+        while(x > 0){
+            if (x > 2){
+                System.out.print("a");
+            }
+            x = x -1;
+            System.out.print("-");
+            if (x == 2){
+                System.out.print("b c");
+            }
+            if (x == 1 ){
+                System.out.print("d");
+                x = x - 1;
+            }
+        }
+    }
+}
